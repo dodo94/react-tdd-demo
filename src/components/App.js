d3b302b559c8d044eb13e931b6c7192ce36d11d7
dodo94 <dodo94.cntt@gmail.com>
2021-05-05 23:41:19 +0700

    Add cypress

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-    return <div className="hello">Hello, World</div>;
+    return <div className="hello">Hello, world!</div>;
 }
 
 export default App;
